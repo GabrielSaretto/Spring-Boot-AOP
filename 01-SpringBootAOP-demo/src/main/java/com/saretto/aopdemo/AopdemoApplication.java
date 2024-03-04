744ba2de66165f86cc4be1d312db7e9d68c539cc
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-03-04 14:12:07 -0300

    Create Target Object

--- 01-SpringBootAOP-demo/src/main/java/com/saretto/aopdemo/AopdemoApplication.java
+++ 01-SpringBootAOP-demo/src/main/java/com/saretto/aopdemo/AopdemoApplication.java
@@ -1,7 +1,10 @@
 package com.saretto.aopdemo;
 
+import com.saretto.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopdemoApplication {
@@ -10,4 +13,18 @@ public class AopdemoApplication {
 		SpringApplication.run(AopdemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO){
+
+		return runner -> {
+
+			demoTheBeforeAdvice(theAccountDAO);
+
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+		// call the business method
+		theAccountDAO.addAccount();
+	}
 }
---
+++ 01-SpringBootAOP-demo/src/main/java/com/saretto/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,6 @@
+package com.saretto.aopdemo.dao;
+
+public interface AccountDAO {
+
+    void addAccount();
+}
