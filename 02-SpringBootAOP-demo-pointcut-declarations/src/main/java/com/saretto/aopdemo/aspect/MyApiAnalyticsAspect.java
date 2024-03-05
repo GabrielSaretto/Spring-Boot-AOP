cf46ed27eb01e082d54a0130f278d10564c1e027
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-03-05 13:24:16 -0300

    Read Method Arguments with JoinPoints

--- 02-SpringBootAOP-demo-pointcut-declarations/src/main/java/com/saretto/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ 02-SpringBootAOP-demo-pointcut-declarations/src/main/java/com/saretto/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -1,7 +1,10 @@
 package com.saretto.aopdemo.aspect;
 
+import com.saretto.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +14,29 @@ import org.springframework.stereotype.Component;
 public class MyApiAnalyticsAspect {
 
     @Before("com.saretto.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void performApiAnalytics(){
+    public void performApiAnalytics(JoinPoint theJoinPoint){
         System.out.println("\n=====>>> Performing API analytics");
+
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+        System.out.println("Method: " + methodSignature);
+
+        // display the method arguments
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop thru args
+        for (Object tempArg : args){
+            System.out.println(tempArg);
+
+            if (tempArg instanceof Account){
+
+                // downcast and print Account specific stuff
+                Account theAccount = (Account) tempArg;
+
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account level: " + theAccount.getLevel());
+            }
+        }
     }
+
 }
