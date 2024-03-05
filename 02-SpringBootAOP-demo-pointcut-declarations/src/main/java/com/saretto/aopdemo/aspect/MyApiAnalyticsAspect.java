5b84437b154662e2218f93d3b28ae0691e25b0d2
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-03-05 11:20:44 -0300

    Ordering Aspects

--- 02-SpringBootAOP-demo-pointcut-declarations/src/main/java/com/saretto/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ 02-SpringBootAOP-demo-pointcut-declarations/src/main/java/com/saretto/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,18 +2,16 @@ package com.saretto.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(2)
-public class MyDemoLoggingAspect {
+@Order(3)
+public class MyApiAnalyticsAspect {
 
     @Before("com.saretto.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice(){
-        System.out.println("\n=====>>> Executing @Before advice on method");
+    public void performApiAnalytics(){
+        System.out.println("\n=====>>> Performing API analytics");
     }
-
 }
