f6adb3dd46d3779afff7488617d68e7d8ca62730
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-03-05 20:07:02 -0300

    @After Advice

--- 02-SpringBootAOP-demo-pointcut-declarations/src/main/java/com/saretto/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 02-SpringBootAOP-demo-pointcut-declarations/src/main/java/com/saretto/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -14,6 +14,14 @@ import java.util.List;
 @Order(2)
 public class MyDemoLoggingAspect {
 
+    @After("execution(* com.saretto.aopdemo.dao.AccountDAO.findAccounts(..))")
+    public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint){
+
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n======>>> Executing @After (finally) on method: " + method);
+    }
+
     @AfterThrowing(
             pointcut = "execution(* com.saretto.aopdemo.dao.AccountDAO.findAccounts(..))",
             throwing = "theExc")
