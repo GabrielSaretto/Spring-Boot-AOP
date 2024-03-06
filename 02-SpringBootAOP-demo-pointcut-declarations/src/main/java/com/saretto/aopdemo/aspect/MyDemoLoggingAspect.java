279d76cbef65a5752659b396ee469b2b32895a6c
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-03-06 14:27:11 -0300

    @Around Advice - Rethrow Exception

--- 02-SpringBootAOP-demo-pointcut-declarations/src/main/java/com/saretto/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 02-SpringBootAOP-demo-pointcut-declarations/src/main/java/com/saretto/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -28,6 +28,7 @@ public class MyDemoLoggingAspect {
 
         // now, let's execute the method
         Object result = null;
+
         try {
             result = theProceedingJoinPoint.proceed();
         }
@@ -35,8 +36,8 @@ public class MyDemoLoggingAspect {
             // log the exception
             System.out.println(exc.getMessage());
 
-            // give user a custom message
-            result = "Major accident! But no worries, your private AOP helicopter is on the way!";
+            // rethrow exception
+            throw exc;
         }
 
         // get end timestamp
